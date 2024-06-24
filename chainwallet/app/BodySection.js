67c9a39b96073ed27fbfcac42fc63a6e13825457
jobex11 <joblawal33@gmail.com>
2024-06-24 13:56:12 +0100

    footer

--- chainwallet/app/BodySection.js
+++ chainwallet/app/BodySection.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import { Button } from "@mui/material";
 const BodySection = () => {
-  return <div>BodySection</div>;
+  return (
+    <div>
+      <div>
+        <h4>
+          USD <span>ETH</span>
+        </h4>
+        <h1>$0.00</h1>
+        <h3>0.0000</h3>
+        <Button>
+          <span>Swap</span>
+          <SwapHorizIcon />
+        </Button>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default BodySection;

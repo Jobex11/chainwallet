4703e4339ad27befd64502ab40ff5d5606b8bc82
jobex11 <joblawal33@gmail.com>
2024-06-24 17:31:09 +0100

    good

--- chainwallet/app/page.js
+++ chainwallet/app/page.js
@@ -6,6 +6,27 @@ import Footer from "./Footer";
 const Home = () => {
   return (
     <div className="bg-gray-100 flex items-center justify-center min-h-screen min-w-screen">
+      <div className="grid grid-rows-[auto_1fr_auto] h-screen w-screen sm:h-auto sm:w-auto sm:max-h-[600px] sm:max-w-[450px] bg-customBlack shadow-lg rounded-lg">
+        <Navbar />
+        <BodySection />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
+{
+  /*
+import React from "react";
+import Navbar from "./Navbar";
+import BodySection from "./BodySection";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <div className="bg-gray-100 flex items-center justify-center min-h-screen min-w-screen">
       <div className="grid grid-rows-[auto_1fr_auto] h-screen w-screen sm:h-[600px] sm:w-[450px] bg-customBlack shadow-lg rounded-lg">
         <Navbar />
         <BodySection />
@@ -16,3 +37,6 @@ const Home = () => {
 };
 
 export default Home;
+  
+  */
+}

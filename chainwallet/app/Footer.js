d8bf06e53337a4eec6101567cfcb29af3ad45518
jobex11 <joblawal33@gmail.com>
2024-06-24 13:05:41 +0100

    navbar

--- chainwallet/app/Footer.js
+++ chainwallet/app/Footer.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="p-3 text-white flex items-center justify-between border-t border-gray-400 shadow-sm">
+      <div>
+        <h1>Home</h1>
+      </div>
+      <div>
+        <h1>Wallet</h1>
+      </div>
+      <div></div>
+      <div>
+        <h1>Earn</h1>
+      </div>
+      <div>
+        <h1>Earn</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- chainwallet/app/Navbar.js
+++ chainwallet/app/Navbar.js
@@ -1,14 +1,52 @@
+"use client";
+import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import Button from "@mui/material/Button"; // Import Button from Material-UI
+import SyncIcon from "@mui/icons-material/Sync";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 const Navbar = () => {
   return (
-    <div className="border-b border-gray-400 shadow-sm">
-      <div>in process...</div>
-      <div></div>
+    <div className="p-3 text-gray-400 text-lg flex items-center justify-between border-b border-gray-500 shadow-sm">
+      <div className="flex flex-row gap-2">
+        <div className="rounded-full border border-1">
+          h
+          {/*
+          <Image
+            src="https://th.bing.com/th/id/OIP.QjynegEfQVPq5kIEuX9fWQHaFj?rs=1&pid=ImgDetMain"
+            alt="Example Image"
+            width={100}
+            height={50}
+            layout="responsive" />
+          */}
+        </div>
+        <div>
+          <section className="flex">
+            <h1 className="text-gray-100">0x123..5678</h1>
+            <ContentCopyIcon className="" />
+          </section>
+          <section className="text-sm">Morphius</section>
+        </div>
+      </div>
+      <div className="flex gap-4 text-bold">
+        <Button className="rounded-2xl bg-gray-800 opacity-1 text-sm ">
+          <div className="flex justify-center gap-1 text-yellow-500 text-bold">
+            <LockOpenIcon fontSize="small" />
+            <h2 className="text-base">BETA</h2>
+          </div>
+        </Button>
+        <Link href="/" className="hover:text-yellow-700">
+          <ExitToAppIcon />
+        </Link>
+        <Link href="/" className="hover:text-yellow-700">
+          <SyncIcon />
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default Navbar;
-`

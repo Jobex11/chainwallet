6ed70d5bb174036538d69e5421d4764a50c79be4
jobex11 <joblawal33@gmail.com>
2024-06-24 07:27:57 +0100

    changes

---
+++ chainwallet/app/Navbar.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="border-b border-gray-400 shadow-sm">
+      <div>in process...</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
+`
--- chainwallet/tailwind.config.js
+++ chainwallet/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        customBlack: "#1a1a1a", // Example of a custom black color
+      },
     },
   },
   plugins: [],
